909b35347fafb316b82fd9891218c5f065a9f09f
A2528 <60137575+A2528@users.noreply.github.com>
2020-06-02 17:53:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+var Car_1, Car_2, Car_3, Car_4;
+var cars, car1, car2, car3, car4;
+var track
+
+function preload() {
+  Car_1 = loadImage("../images/car1.png")
+  Car_2 = loadImage("../images/car2.png")
+  Car_3 = loadImage("../images/car3.png")
+  Car_4 = loadImage("../images/car4.png")
+  track = loadImage("../images/track.jpg")
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if (gameState==2) {
+    game.end();
+  }
+}
+
+
